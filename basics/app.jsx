417854fbe0e4c0bdf9e095a10be75f2000a416df
amarinas <ali.marinas@icloud.com>
2017-11-25 10:29:27 -0800

    init

---
+++ basics/app.jsx
@@ -0,0 +1,9 @@
+function Application(){
+  return(
+    <div>
+      <h1>React time!</h1>
+      <p>what what </p>
+    </div>
+  );
+}
+ReactDOM.render(<Application />, document.getElementById('container'));
