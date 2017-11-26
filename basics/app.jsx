d8afefff2634f053ef5385646e74da718fb9125a
amarinas <ali.marinas@icloud.com>
2017-11-26 00:21:42 -0800

    add title my scoreboard

--- basics/app.jsx
+++ basics/app.jsx
@@ -1,9 +1,24 @@
-function Application(){
-  return(
-    <div>
-      <h1>React time!</h1>
-      <p>what what </p>
-    </div>
+function Application(props) {
+  return (
+    <div className="scoreboard">
+      <div className="header">
+        <h1> {props.title}</h1>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+          Jom Player
+        </div>
+          <div className="score">
+            <div className="counter">
+              <button className="counter-action-decrement"> - </button>
+              <div className="counter-score"> 31 </div>
+              <button className="counter-action-increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
+   </div>
   );
 }
-ReactDOM.render(<Application />, document.getElementById('container'));
+ReactDOM.render(<Application title="My Scoreboard"/>, document.getElementById('container'));
