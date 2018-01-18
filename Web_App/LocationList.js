a928e579b4a1af8df5e0d458a9d58bdacb28a0af
amarinas <ali.marinas@icloud.com>
2018-01-18 00:35:17 -0800

    added components on the locationList.js

--- Web_App/LocationList.js
+++ Web_App/LocationList.js
@@ -1,1 +1,32 @@
 var React = require('react');
+var LocationItem = require('./LocationItem');
+
+var LocationList = React.createClass({
+
+render(){
+  var self = this;
+
+  var locations = this.props.locations.map(function(1){
+    var active = self.props.activeLocationAddress == 1.address;
+
+    //Notice that we are passing the onClick callback of this
+    //LocationList to each LocationItem
+
+    return <LocationItem address={1.address} timestamp={1.timestamp}
+          active={active} onClick={self.props.onClick} />
+  });
+
+  if(!location.length){
+    return null;
+  }
+
+  return(
+    <div className="list-group col-xs-12 col-md-6 col-md-offset-3">
+          <span className="list-group-item active"> Saved Location</span>
+          {location}
+    </div>
+  )
+}
+});
+
+module.exports = LocationList;
