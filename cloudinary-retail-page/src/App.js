3f9050765e609c9ac80582a12ee6d88b04948d3c
amarinas <ali.marinas@icloud.com>
2018-01-28 09:26:21 -0800

    update

--- cloudinary-retail-page/src/App.js
+++ cloudinary-retail-page/src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import { Image, CloudinaryContext, Transformation} from 'cloudinary-react';
 import logo from './logo.svg';
 import './App.css';
 
+const ImageTransformations = ({width, selectedShirt}) => {
+  return (
+    <Image publicId={selectedShirt.main+'.jpg'}>
+        <Transformation width={width} crop="scale" />
+    </Image>
+  );
+};
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    const defaultShirt = {id: 1, main: 'shirt_only'};
+    this.state = {
+      shirt:[
+        defaultShirt,
+        {id: 2, main: 'laying-shirt'},
+        {id: 3, main: 'hanging_t-shirt'}
+      ],
+      selectedShirt: defaultShirt
+    };
+  }
+
+  selectShirt(thumb){
+    this.setState({selectedShirt:thumb}, _ => this.forceUpadate())
+  }
+
+
   render() {
     return (
       <div className="App">
