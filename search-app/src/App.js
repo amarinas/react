8c256a25d88e2022d9a5370a5a46f8ab0ba097e9
amarinas <ali.marinas@icloud.com>
2018-02-14 22:25:13 -0800

    added search box and result list

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Jumbotron, Grid} from 'react-bootstrap';
+import SearchForm from './components/SearchForm';
+import Result from './components/result';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Jumbotron>
+          <Grid>
+            <h1> Search App</h1>
+            <p> this is a simple Search App </p>
+            <SearchForm />
+
+          </Grid>
+        </Jumbotron>
+        <Result />
+
       </div>
     );
   }
