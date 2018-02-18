c037a637864d302f2055cba9808ba9596b25d3f8
amarinas <ali.marinas@icloud.com>
2018-02-17 18:21:28 -0800

    added components for guesLists

--- rsvp/src/App.js
+++ rsvp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import GuestList from './GuestList'
 
 class App extends Component {
 
@@ -23,7 +24,7 @@ class App extends Component {
 
   //getUnconfirmedGuests = () =>
 
-  
+
   render() {
     return (
       <div className="App">
@@ -58,32 +59,7 @@ class App extends Component {
               </tr>
             </tbody>
           </table>
-          <ul>
-            <li className="pending"><span>Safia</span></li>
-            <li className="responded"><span>Iver</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li className="responded">
-              <span>Corrina</span>
-              <label>
-                <input type="checkbox" checked /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-            <li>
-              <span>Joel</span>
-              <label>
-                <input type="checkbox" /> Confirmed
-              </label>
-              <button>edit</button>
-              <button>remove</button>
-            </li>
-          </ul>
+          <GuestList />
         </div>
       </div>
     );
