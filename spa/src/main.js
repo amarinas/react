898379d01c50cec9ae33b0f28f80300f35d21572
amarinas <ali.marinas@icloud.com>
2018-02-19 18:00:49 -0800

    set up vue-router

--- spa/src/main.js
+++ spa/src/main.js
@@ -1,15 +1,42 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the import command
+// runtime-only or standalone has been set in webpack.base.conf with an alias
+// import the vue instance
 import Vue from 'vue'
+
+//import the app component
 import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
+//import the vue router
+import VueRouter from  'vue-router'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+//tell vue to use the router
+
+Vue.use(VueRouter)
+
+//define your Routes
+
+const routes = []
+
+//Create the router instance and pass the routes option
+//you can pass in additional options here, but lets keep it simple for now
+
+const router = new VueRouter({
+  routes,
+  mode: history
+
 })
+
+new Vue({
+  //define the selector for the root component
+  el: '#app',
+
+  //pass the template to the root component
+  template: '<App/>',
+
+  //declare components that the root component can access
+  components: { App },
+
+  //pass in the router to the Vue instance
+  router
+
+}).$mount('#app') // mount the router on the app
